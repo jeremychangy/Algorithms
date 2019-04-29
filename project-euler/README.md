0039094f2daff28469fd7fe0da45ee6b4b072757
moldingtofu <jeremykchang@gmail.com>
2019-04-28 23:20:31 -0400

    started problem 3

--- project-euler/README.md
+++ project-euler/README.md
@@ -1,2 +1,3 @@
 #Project Euler Problems
+https://projecteuler.net
 Math and python practice
---
+++ project-euler/p3.py
@@ -0,0 +1,6 @@
+def maxPrimeFactor(x):
+  pass
+
+if __name__ == "__main__":
+  n = 600851475143
+  print(maxPrimeFactor(n))
